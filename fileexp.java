72fd86dda95d710600c78e01e0b16aeb26ee5e6b
R-a-j-e-s-h-M <>
2022-02-26 16:47:29 +0530

    Experiment 6

---
+++ fileexp.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+class fileexp
+{
+ public static void main(String args[])
+ {
+  System.out.println("Opening file..");
+  try{
+  FileInputStream f= new FileInputStream("test.txt");
+  System.out.println("File opened");
+  }
+  catch(FileNotFoundException e)
+  {
+   System.out.println(e);
+  } 
+ }
+}
+
+
+
